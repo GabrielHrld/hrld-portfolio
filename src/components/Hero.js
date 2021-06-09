2c8f9c11b8463df17612df09efd8cf2be0cbc7e6
Gabi <g.federico.rodriguez@gmail.com>
2021-06-09 17:51:11 -0300

    hero effects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,17 +10,28 @@ const Hero = () => {
       <div className="hero-container" id="hero">
         <div className="hero-principal">
           <div className="hero-principal_text">
-            <h1>
-              Gabriel Rodriguez <span>dev.</span>
+            <h1 className="animate__animated animate__fadeInLeft">
+              Gabriel Rodriguez{' '}
+              <span className="animate__animated animate__fadeInLeft animate__delay-1s">
+                dev.
+              </span>
             </h1>
-            <p>Portafolio</p>
+            <p className="animate__animated animate__fadeInLeft animate__delay-2s">
+              Portafolio
+            </p>
           </div>
         </div>
         <div className="hero-secondary">
           <div className="hero-secondary_text">
-            <h1>Flexibilidad</h1>
-            <h2>Creatividad</h2>
-            <h3>Eficacia</h3>
+            <h1 className="animate__animated animate__fadeInRight">
+              Flexibilidad
+            </h1>
+            <h2 className="animate__animated animate__fadeInRight animate__delay-1s">
+              Creatividad
+            </h2>
+            <h3 className="animate__animated animate__fadeInRight animate__delay-2s">
+              Eficacia
+            </h3>
           </div>
         </div>
       </div>
