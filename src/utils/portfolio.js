a1f0882ae7298050dd5bc42bfb9953bc23a23ac2
Gabi <g.federico.rodriguez@gmail.com>
2021-06-28 00:41:29 -0300

    portfolio modified

---
+++ src/utils/portfolio.js
@@ -0,0 +1,36 @@
+export const portfolio = [
+  {
+    title: 'HRLD-merce',
+    snippet:
+      'Ecommerce básico completo desarrollado en React como tecnología frontend, y NestJS junto con Node como tecnología de backend.',
+    link: 'https://hrld-merce.vercel.app/',
+    image: 'https://i.imgur.com/OEqSziK.png',
+  },
+  {
+    title: 'HRLD-jobs',
+    snippet:
+      'Job-bank en progreso. Desarrollado en React como tecnología frontend, y ExpressJS junto con Node como tecnología de backend.',
+    link: 'https://hrld-jobs.vercel.app/',
+    image: 'https://i.imgur.com/gJTmaRu.png',
+  },
+  {
+    title: 'Notes App',
+    snippet:
+      'Pequeña aplicación en la cual podemos registrar nuestras notas, editarlas o eliminarlas. Desarrollado en Node utilizando Express como tecnología de backend.',
+    link: 'http://ec2-3-141-196-58.us-east-2.compute.amazonaws.com/',
+    image: 'https://i.imgur.com/U5Wqha7.png',
+  },
+  {
+    title: 'Cripto Exchange',
+    snippet:
+      "Trackea el precio de las veinte criptomonedas más conocidas, así como su precio y reporta los mejores exchange's en los cuales podemos adquirirlas.",
+    link: 'https://exchange-hrld.vercel.app/',
+    image: 'https://i.imgur.com/8tfw2vk.png',
+  },
+  {
+    title: 'Traveleter',
+    snippet: 'Web layout diseñado para una empresa de viajes.',
+    link: 'https://traveleter.vercel.app/',
+    image: 'https://i.imgur.com/cxfpNvJ.png',
+  },
+];
